403a730f5a2f50470c74e1870629adad7d44fcdb
Carlos H Pinheiro <carloshenriquepinheirodasilva@gmail.com>
2025-01-27 18:43:04 -0300

    adiciona função `sortearAmigo`

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ function limparCampo() {
     campo.value = '';
 };
 
+function sortearAmigo() {
+    let resposta = document.getElementById('resultado');
+
+    if (participantes == '') {
+        resposta.innerHTML = 'Adicione novos participantes para realizar o sorteio.';
+    } else {
+        let indexAleatorio = Math.floor(Math.random() * participantes.length);
+        console.log('index ' + indexAleatorio);
+        let amigoSorteado = participantes[indexAleatorio];
+        resposta.innerHTML = `Seu amigo secreto é ${amigoSorteado}`;
+        participantes.splice(indexAleatorio, 1);
+    };
+};
+
 function listarAmigos() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';   // Limpa o conteúdo da lista no HTML
